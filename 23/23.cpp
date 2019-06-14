f4fe7de9cc4c3a84a33a0d0bd72735a0244ff09c
freehawkzk <freehawkzk@126.com>
2019-06-14 09:14:01 +0800

    23 合并k个有序链表
    代码注释

--- 23/23.cpp
+++ 23/23.cpp
@@ -2,6 +2,7 @@
 #include <map>
 #include <vector>
 
+//链表节点定义
 struct ListNode
 {
     int val;
@@ -10,17 +11,21 @@ struct ListNode
 
 };
 
+//合并K个链表
 ListNode* mergeKLists(std::vector<ListNode*>& lists) 
 {
     if (lists.size() == 0)
         return 0;
     if (lists.size() == 1)
         return lists[0];
+    //主要思路是利用std::map的key是经过排序的这一属性，将所有节点
+    //添加到一个map中，在map中将各个节点的next指针调整好，最终输出的时候直接
+    //返回map的初始节点的数组的第一个元素即可。
     std::map<int, std::vector<ListNode*> > tempmap;
-    for (int i = 0; i < lists.size(); i++)
+    for (int i = 0; i < lists.size(); i++)//遍历K个链表
     {
         ListNode* pList = lists[i];
-        while (pList != 0)
+        while (pList != 0)//将第i个链表所有的元素存入map
         {
             int mapkey = pList->val;
             ListNode* mapvalue = pList;
@@ -28,25 +33,28 @@ ListNode* mergeKLists(std::vector<ListNode*>& lists)
             pList = pList->next;
             mapvalue->next = 0;
             std::map<int, std::vector<ListNode*> >::iterator itor = tempmap.find(mapkey);
-            if (itor == tempmap.end())
+            if (itor == tempmap.end())//当前链表节点的值在map中不存在，则map新增节点
             {
                 std::vector<ListNode*> arr;
                 arr.push_back(mapvalue);
                 tempmap.insert(std::make_pair(mapkey, arr));
             }
-            else
+            else//当前值已经在map中存在，则直接将当前节点的存入对应的map
             {
                 itor->second.push_back(mapvalue);
+                //调整map节点中的链表节点的next指针
                 itor->second[itor->second.size() - 2]->next = itor->second[itor->second.size() - 1];
                 itor->second[itor->second.size() - 1]->next = 0;
             }
 
         }
     }
-    if (tempmap.empty())
+    if (tempmap.empty())//对于输入的是多个空链表时，返回空
     {
         return 0;
     }
+
+    //将map的各个节点中链表节点数组的最后一个节点的next指针指向下一节点数组的第一个元素
     std::map<int, std::vector<ListNode*> >::iterator itorend = tempmap.end();
     itorend--;
     for (std::map<int, std::vector<ListNode*> >::iterator itor = tempmap.begin(); itor != itorend; itor++)
@@ -56,6 +64,7 @@ ListNode* mergeKLists(std::vector<ListNode*>& lists)
         itor--;
         itor->second[itor->second.size() - 1]->next = itor1->second[0];
     }
+    //返回头元素的数组的第一个元素
     ListNode* head = 0;
     head = tempmap.begin()->second[0];
     return head;
